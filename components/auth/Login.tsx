ea00f0049cc495da8d02614b0cf02593b113376b
RiugeBrian <riungebrayandev@gmail.com>
2023-09-23 21:09:57 +0300

    Authentication

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -5,7 +5,8 @@ export default function Login(){
        <li className="list-none mt-5">
         <button onClick={()=> signIn()} 
         className="text-sm px-6 py-2 rounded-xl  text-white bg-gray-500 disabled:opacity-25 "
-        >Sign In</button>
+        >Sign In made same changes here
+        </button>
        </li>
     )
 }
