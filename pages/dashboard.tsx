9a338c9faf276f5152cae0361aa8bb159e1e3eed
RiugeBrian <riungebrayandev@gmail.com>
2023-10-05 16:15:48 +0300

    dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,16 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]"
-import { redirect } from "next/dist/server/api-utils"
+import { redirect } from "next/navigation"
 
 
 export default async function Dashboard(){
     const session = await getServerSession(authOptions)
     if(!session){
-       
+       redirect("/api/auth/signin")
     }
     return(
         <div>
-            <h1 className="text-sm "></h1>
+            <h1 className="text-sm ">welcome back {session.user?.name}</h1>
         </div>
     )
 }
