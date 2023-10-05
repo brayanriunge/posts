081f8d5138d68930d19d22d046b2de4936b7b40f
RiugeBrian <riungebrayandev@gmail.com>
2023-10-05 16:35:06 +0300

    dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,22 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./api/auth/[...nextauth]"
 import { redirect } from "next/navigation"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 
 export default async function Dashboard(){
-    const session = await getServerSession(authOptions)
+    const{data: session} = useSession()
+    const router = useRouter()
+   useEffect(()=>{
     if(!session){
-       redirect("/api/auth/signin")
+        window.location.href= "/api/auth/signin"
     }
+   },[session])
     return(
         <div>
-            <h1 className="text-sm ">welcome back {session.user?.name}</h1>
+            <h1 className="text-sm ">welcome back {session?.user?.name}</h1>
         </div>
     )
 }
