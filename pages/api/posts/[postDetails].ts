ff4fc71d581e741d7f9eb41d479663e4854520ba
RiugeBrian <riungebrayandev@gmail.com>
2023-10-15 14:18:46 +0300

    building comments

--- pages/api/posts/[postDetails].ts
+++ pages/api/posts/[postDetails].ts
@@ -1,19 +1,38 @@
 
 import { prisma } from "@/util/db";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ){
     if(req.method === "GET"){
+        const session = await getServerSession(req, res, authOptions)
+        if(!session){
+            return res.status(401).json({message:"You must be logged in first."})
+        }
         try {
           const postId = req.query.slug as string  
+
           const data = await prisma.post.findUnique({
             where: {
                 id: postId
+            },
+            include:{
+                user:true,
+                comment:{
+                    orderBy:{
+                        createdAt:"desc"
+                    },
+                    include:{
+                        user:true
+                    }
+                }
             }
           })
+          res.status(200).json(data)
         } catch (error) {
             res.status(403).json({message: " error fetching comments"})
         }
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,21 +1,26 @@
 
 import Layout from "@/components/Layout"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { PostData } from ".."
 
-const fetchDetails = async (slug: string)=>{
-    try {
-     const response = await fetch(`http://localhost:3000/api/posts/${slug}`)
-     const data = response.json()
-     console.log(data)
-    } catch (error) {
-        console.error("Error fetching Comments", error)
-    }
-}
 
 export default function PostDetails(){
     const router = useRouter()
     const{slug} = router.query
+    const [postData, setPostData]= useState<PostData[]>([])
+
+    const fetchDetails = async (slug: string)=>{
+        try {
+         const response = await fetch(`http://localhost:3000/api/posts/${slug}`)
+         const data = await response.json()
+         console.log(data)
+         setPostData(data)
+        } catch (error) {
+            console.error("Error fetching Comments", error)
+        }
+    }
+    
     useEffect(()=>{
         if(slug){
             fetchDetails(slug as string)
